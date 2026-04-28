eb665564594a9d28871ff31603fa6853f40128eb
krisztavasas <kriszta.vasas@rabit.hu>
2026-04-28 14:31:39 +0200

    Security fix

--- backend/src/main/java/com/krisztavasas/db_library/config/SecurityConfig.java
+++ backend/src/main/java/com/krisztavasas/db_library/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/actuator/health/**").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
